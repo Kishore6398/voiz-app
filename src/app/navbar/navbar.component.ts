681d5ebe426d3b1dc1bee6bf335ee4bf18544b2c
Vijay Krishnamoorthy <vijaytoinbox@gmail.com>
2019-08-04 02:34:25 +0530

    Upfrading the dashboard components

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
+	testForm: FormGroup;
+	  optionsSelect = [
+	    { value: '1', label: 'Option 1' },
+	    { value: '2', label: 'Option 2' },
+	    { value: '3', label: 'Option 3' },
+	  ];
   constructor() { }
 
   ngOnInit() {
-  }
+    this.testForm = new FormGroup({
+      testSelect: new FormControl('1')
+    });
 
+}
 }
