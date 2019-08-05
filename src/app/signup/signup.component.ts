96a6dc9ca5da85da041d82bc2a360a337f11352f
Vijay Krishnamoorthy <vijaytoinbox@gmail.com>
2019-08-05 23:34:07 +0530

    Added front-end validation
    Added front end validation part for the signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  constructor() { }
-
+  constructor(private fb: FormBuilder) { }
+  registerForm: FormGroup;
   ngOnInit() {
+  this.registerForm = new FormGroup({
+  	emailInput : new FormControl(null, [Validators.required, Validators.email]),
+  	mobileInput: new FormControl(null, Validators.required),
+  	passwordInput: new FormControl(null, Validators.required),
+  	repasswordInput: new FormControl(null, Validators.required),
+  });
+  //console.log(this.registerForm);
   }
 
+  get emailInput() { return this.registerForm.get('emailInput'); }
+  get mobileInput() { return this.registerForm.get('mobileInput'); }
+  get passwordInput() { return this.registerForm.get('passwordInput'); }
+  get repasswordInput() { return this.registerForm.get('repasswordInput') }
 }
